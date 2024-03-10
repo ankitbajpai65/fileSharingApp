403be94685c1a9cf47bba3638438a3fbcec027cf
Ankit Bajpai <ankit65bajpai@gmail.com>
2024-03-10 23:37:02 +0530

    Updated email controller

--- server/controllers/email.js
+++ server/controllers/email.js
@@ -3,7 +3,19 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
+const SIZE_UNITS = ['bytes', 'KB', 'MB', 'GB', 'TB'];
+
 const handleSendEmail = (req, res) => {
+    let fileSize = req.body.fileSize;
+    let count = 0;
+
+    while (fileSize >= 1024 && count < SIZE_UNITS.length - 1) {
+        fileSize /= 1024;
+        count++;
+    }
+
+    const formattedFileSize = `${fileSize.toFixed(2)} ${SIZE_UNITS[count]}`;
+
     try {
         sendEmail({
             subject: `${req.body.user} shared a file link with you`,
@@ -14,7 +26,7 @@ const handleSendEmail = (req, res) => {
             <p>Hi there,</p>
             <p>Please visit the below attached link to download the file -</p>
             <p><a href="${req.body.fileLink}">Download file</a></p>
-            <p>File size - ${req.body.fileSize} bytes</p>
+            <p>File size - ${formattedFileSize}</p>
             <p>Thank you for using filegem.</p>
         `
         });
